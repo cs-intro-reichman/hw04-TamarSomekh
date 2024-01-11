c3eecc764013879020c26af0bfab32306b4a385c
TamarSomekh <tamar.somekh@post.runi.ac.il>
2024-01-12 01:17:38 +0200

    hm uplaod

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,146 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        System.out.println(findMissingInt(new int[] {0, 1, 2, 3, 4, 6}));
+        System.out.println(secondMaxValue(new int[] {6, 9, 4, 7, 3, 4}));
+        System.out.println(containsTheSameElements(new int[] {2, 2, 3, 7, 8, 3, 2}, new int[] {8, 2, 7, 7, 3}));
+        System.out.println(isSorted(new int[] {1, -2, 3}));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int missing = 0;
+        int min = minVal(array);
+        int max = maxVal(array);
+        double bigSum = ((double)(array.length + 1) / 2 ) * (min + max);
+        int sum = sum(array);
+        missing = (int) bigSum - sum;
+
+        return missing;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        //finding the max value
+        int max = maxVal(array);
+     
+        //removing the max value from array
+        int[] newArr = new int[array.length - 1];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != max){
+                newArr[index] = array[i];
+                index++;
+            }
+        }
+
+        //finding the second max value
+        int secondMax = maxVal(newArr);
+
+        return secondMax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        //going through the arrays 
+        //and using the function contains to check if the values are the same
+        boolean areSame1 = true;
+        for (int i = 0; i < array1.length; i++){
+            if (contains(array2, array1[i]) == true) {
+                areSame1 = true;
+            }else{
+                areSame1 = false;
+                break;
+            }
+        }
+
+        boolean areSame2 = true;
+        for (int i = 0; i < array2.length; i++){
+            if (contains(array1, array2[i]) == true) {
+                areSame2 = true;
+            }else{
+                areSame2 = false;
+                break;
+            }
+        }
+        
+        if (areSame1 == areSame2) {
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
+        boolean goingUp = true;
+        for (int i = 0; i < array.length; i++){
+            if (i == array.length - 1) {
+                goingUp = true;
+            }else{
+                if (array[i] <= array[i+1]) {
+                    goingUp = true;
+                }else{
+                    goingUp = false;
+                    break;
+                }
+            }  
+        }
+
+        boolean goingDown = true;
+        for(int i = 0; i < array.length; i++){
+            if (i == array.length -1) {
+                goingDown = true;
+            }else{
+                if (array[i] >= array[i+1] ) {
+                    goingDown = true;
+                }else{
+                    goingDown = false;
+                    break;
+                }
+            }
+        }
+        
+        if (goingDown == true || goingUp == true) {
+            return true;
+        }else{
+            return false;
+        }  
+    }
+
+
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+
+    public static boolean contains(int[] array, int value){
+        //a function that checks if an array contains a value
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
         return false;
     }
 
+    //function that finds maximum value in an array
+    public static int maxVal(int[] array){
+        int max = array[0];
+         for(int i = 0; i < array.length; i++){
+            if ((array[i] > max)) {
+                max = array[i];
+            }
+        }
+        return max; 
+    }
+    //function that finds minimum value in an array
+    public static int minVal(int[] array){
+        int min = array[0];
+        for(int i = 0; i < array.length; i++){
+            if ((array[i] < min)) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,184 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        //System.out.println(capVowelsLowRest("vowels are fun"));
+        //System.out.println(lowerCase("TAMAR"));
+        //System.out.println(UpperThenLower("TAmarSOmekh"));
+        System.out.println(camelCase("     Tamar        Somekh hamuda       VEMETUKA wow"));
+        allIndexOf("Hello world",'l');
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String ans = "";
+        char ch = ' ';
+
+        for(int i = 0; i < string.length(); i++){
+            ch = string.charAt(i);
+            //checking if ch is a spacer
+            if (ch == 32) {
+                ans = ans + (char)(ch);
+            //checking if ch is a vowel
+            }else if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U') {
+                //checking if its uppercase
+                if (ch <= 'A' && ch >= 'Z') {
+                    ans = ans + (char)(ch);
+                //if not uppercase - changing it
+                }else{
+                    ans = ans + (char)(ch - 32);
+                }
+            }else{
+                //if the regular letter is uppercase - lower it
+                if (ch <= 'A' && ch >= 'Z') {
+                    ans = ans + (char)(ch + 32);
+                }else{
+                    ans = ans + (char)(ch);
+                }
+            }
+        }
+        return ans;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String ans = "";
+        
+        
+        //finding index of first space
+        int firstSpace = string.indexOf(' ');
+        
+        //cleaning up spaces in begining
+        while (firstSpace == 0) {
+            string = string.substring(1);
+            firstSpace = string.indexOf(' ');
+        }
+        
+        //if the sentence is one word
+        //change it to lowercase and stop
+        if (firstSpace == -1) {
+            ans = lowerCase(string);
+        
+        //changing the first word and saving it in ans
+        }else{
+            //adding the first word to ans
+            String firstWord = string.substring(0, (firstSpace));
+            firstWord = lowerCase(firstWord);
+            ans = ans + firstWord;
+            
+            String restOfSen = string.substring(firstSpace + 1);
+            String currentWord = "";
+            
+            firstSpace = restOfSen.indexOf(' ');
+            boolean moreWords = true;
+            //a loop that will go through the rest of the sentence    
+            while (moreWords == true){
+                //erasing spaces between words
+                if (firstSpace == 0) {
+                    restOfSen = restOfSen.substring(1);
+                }else{
+                    //defining the next word
+                    if (firstSpace != -1) {
+                        currentWord = restOfSen.substring(0, (firstSpace));
+                    //defining what happens in the last word
+                    }else{
+                        currentWord = restOfSen;
+                        moreWords = false;
+                    }
+                    //fixing the letters and adding to ans
+                    currentWord = UpperThenLower(currentWord);
+                    ans = ans + currentWord;
+                    restOfSen = restOfSen.substring(firstSpace + 1); 
+                    
+                }
+                firstSpace = restOfSen.indexOf(' ');
+                
+            }
+        }
+        return ans;
+
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int arraySize = 0;
+        for(int i = 0; i < string.length(); i++){
+            if (string.charAt(i) == chr) {
+                arraySize++;
+            }
+        }
+        int arrayIndex = 0;
+        String str = "output: {";
+        int[] arr = new int[arraySize];
+        for(int j = 0; j < string.length(); j++){
+            if (string.charAt(j) == chr) {
+                arr[arrayIndex] = j;
+                if (arrayIndex != 0){
+                    str = str + ", " ;
+                }
+                str = str + j;
+                arrayIndex++;
+            }
+        }
+        str = str + "}";
+        System.out.println(str);
+        return arr;
+    }
+
+    public static String lowerCase(String s) {
+        
+        String ans = "" ;
+        int i = 0;
+        while (i < s.length()) {
+            char ch = s.charAt(i);
+            //checking if ch is a letter
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                //if the letter is uppercase
+                //changing it to lowercase
+                if (ch >= 'A' && ch <= 'Z') {
+                    ans = ans + (char) (s.charAt(i) + 32); 
+                }
+                else{
+                    ans = ans + ch;
+                }
+                
+            // if ch isnt a letter, add it to the string    
+            } else {
+                ans = ans + ch;
+            }
+            
+            i++;
+        }
+
+        return ans;
+    }
+
+    public static String UpperThenLower (String s){
+        String ans = "" ;
+        if (s.charAt(0) >= 'a' && s.charAt(0) <= 'z' ) {
+            ans = ans + (char) (s.charAt(0) - 32);
+        }else{
+            ans = ans + (char) (s.charAt(0));
+        }
+        
+        int i = 1;
+        while (i < s.length()) {
+            char ch = s.charAt(i);
+            //checking if ch is a letter
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                //if the letter is uppercase
+                //changing it to lowercase
+                if (ch >= 'A' && ch <= 'Z') {
+                    ans = ans + (char) (s.charAt(i) + 32); 
+                }
+                else{
+                    ans = ans + ch;
+                }
+                
+            // if ch isnt a letter, add it to the string    
+            } else {
+                ans = ans + ch;
+            }
+            
+            i++;
+        }
+
+        return ans;
     }
 }
